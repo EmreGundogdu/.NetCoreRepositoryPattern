44cdf98327b3dada62ae0b1ecebd84ad6a9edee7
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-04 00:31:42 +0300

    IUnitOfWork interface added

---
+++ PocketBook/Core/IConfiguration/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using PocketBook.Core.Repositories.Interface;
+
+namespace PocketBook.Core.IConfiguration
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+        Task CompleteAsync();
+
+    }
+}
