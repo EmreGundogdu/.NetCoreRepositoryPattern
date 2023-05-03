5009b7d7feadedc8b71d5ed4e24e1f8aa3e713b9
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-03 23:52:01 +0300

    DbContext added

---
+++ PocketBook/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PocketBook.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
