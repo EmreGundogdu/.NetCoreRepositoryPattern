e8aca53adf33c659bc5668d64ae56c4b15c6f8ae
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-04 00:31:29 +0300

    Repository interface and methods added

---
+++ PocketBook/Core/Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using PocketBook.Models;
+
+namespace PocketBook.Core.Repositories.Interface
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<string> GetFirstNameAndLastName(Guid id);
+    }
+}
