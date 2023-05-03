b35e58da570eaefa49dbb6877ef46c8cd51b8b0f
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-04 00:11:44 +0300

    IGenericRepo interface added

---
+++ PocketBook/Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PocketBook.Core.Repositories
+{
+    public interface IGenericRepository<X> where X : class
+    {
+        Task<IEnumerable<X>> All();
+        Task<X> GetById(Guid id);
+        Task<bool> Add(X entity);
+        Task<bool> Delete(Guid id);
+        Task<bool> Upsert(X entity);
+
+    }
+}
