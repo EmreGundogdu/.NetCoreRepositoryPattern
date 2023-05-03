992b5daef0ec6f4738ba7b1ba1397fd2cf2b0c91
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-04 00:31:49 +0300

    Concrete repository classes added

---
+++ PocketBook/Core/Repositories/Concrete/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PocketBook.Core.Repositories.Interface;
+using PocketBook.Data;
+
+namespace PocketBook.Core.Repositories.Concrete
+{
+    public class GenericRepository<X> : IGenericRepository<X> where X : class
+    {
+        protected AppDbContext context;
+        protected DbSet<X> dbSet;
+        protected readonly ILogger logger;
+        public GenericRepository(AppDbContext context, ILogger logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        public async Task<bool> Add(X entity)
+        {
+            await dbSet.AddAsync(entity);
+            return true;
+        }
+
+        public async Task<IEnumerable<X>> All()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var data = await dbSet.FindAsync(id);
+            dbSet.Remove(data);
+            return true;
+        }
+
+        public async Task<X> GetById(Guid id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public Task<bool> Upsert(X entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PocketBook/Core/Repositories/Concrete/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using PocketBook.Core.Repositories.Interface;
+using PocketBook.Data;
+using PocketBook.Models;
+
+namespace PocketBook.Core.Repositories.Concrete
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context, ILogger logger) : base(context, logger)
+        {
+        }
+
+        public Task<string> GetFirstNameAndLastName(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
