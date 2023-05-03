a1ceddb5c5f21e3dfadb6999afa4731c7835c45f
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-04 00:48:58 +0300

    Services added as DI to IoC

--- PocketBook/Program.cs
+++ PocketBook/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using PocketBook.Core.IConfiguration;
+using PocketBook.Core.Repositories.Concrete;
 using PocketBook.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqlConn"));
 });
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
