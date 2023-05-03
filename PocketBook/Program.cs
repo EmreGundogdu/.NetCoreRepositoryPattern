e3a20c2aed58a5ade50effd9808984644104bd12
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-05-03 23:52:47 +0300

    DbContext added as DI to IoC

--- PocketBook/Program.cs
+++ PocketBook/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PocketBook.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqlConn"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
